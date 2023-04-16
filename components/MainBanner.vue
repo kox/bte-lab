a06d5fbe2554a6d681b6382807a15a061ba8adf8
Inigo <inigo@simplefi.finance>
2023-04-16 23:57:05 +0200

    banner text

--- components/MainBanner.vue
+++ components/MainBanner.vue
@@ -2,11 +2,11 @@
 <template>
   <div class="w-full h-screen bg-cover bg-center bg-black bg-banner-1 scale-100 animate-fade-in"><!-- filter contrast-150 brightness-125 -->
     <div class="flex flex-col text-center items-center justify-center h-full w-full bg-gray-900 bg-opacity-50">
-      <h1 class="flex font-sans text-4xl md:text-6xl xl:text-7xl text-white pl-5 pr-5 leading-snug pb-10">
+      <h1 class="flex font-futura text-4xl md:text-6xl md:font-semibold md:tracking-mid text-white pl-5 pr-5 leading-snug pb-10">
         <div class="leading-tight">BIOMEDICAL & TISSUE<br/>ENGINEERING LABORATORY</div>
       </h1>
       
-      <h2 class="flex font-sans text-3xl md:text-4xl xl:text-5xl text-gray-200 text-opacity-70">ESCOBEDO-LUCEA LAB</h2>
+      <h2 class="flex font-futura text-3xl md:text-4xl md:font-semibold md:tracking-mid text-white text-opacity-70">ESCOBEDO-LUCEA LAB</h2>
     </div>
   </div>
 </template>
