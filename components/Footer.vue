afef234c7c4d3ed7db0ba8a47d203a0de526ad88
Inigo <inigo@simplefi.finance>
2023-04-18 23:34:53 +0200

    Updated footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full flex flex-col bg-white pt-20">
-    <div class="container md:mx-auto md:w-full bg-white">
+    <div class="container md:mx-auto md:w-3/4 xl:w-3/5 bg-white">
       <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 xl:grid-cols-6 gap-6 pb-10 justify-items-center">
         <div v-for="(image, index) in images" :key="index" class="h-4/5 mx-auto">
           <img :src="image.src" :alt="image.alt" :width="image.width" :height="image.height" class="w-full h-full object-cover sm:object-scale-down">
