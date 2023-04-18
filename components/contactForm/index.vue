6d7e8eab9566009d42b845f7462801007c0ba6e9
Inigo <inigo@simplefi.finance>
2023-04-18 23:35:09 +0200

    Updated contact form

--- components/contactForm/index.vue
+++ components/contactForm/index.vue
@@ -1,39 +1,96 @@
 <template>
-    <div class="w-full flex flex-col bg-white pt-72 pb-20">
-        <div class="container mx-auto justify-center">
-            <div class="flex flex-col text-center text-black">
-                <h2 class="text-6xl md:text-7xl font-semibold pb-10 md:pb-12 tracking-tight justify-center">Contact us</h2>
-                
-                <p class="text-2xl">Av. de les Tres Creus, 2, 46014 València, Valencia, Spain</p>
-                <p class="text-2xl"><a href="mailto:example@btelabvalencia@gmail.com">btelabvalencia@gmail.com</a></p>
-                <p class="text-2xl"><a href="tel:+963131893">963 131 893</a></p>
+    <div id="contact">
+
+        <div class="w-full flex flex-col bg-white pt-72 pb-20">
+            <div class="container mx-auto px-4">
+
+                <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+
+                    <div class="p4 text-center md:pr-16">
+                        <h2
+                            class="font-futura text-3xl md:text-5xl font-semibold pb-10 md:pb-12 tracking-tight justify-center antialiased">
+                            Contact us
+                        </h2>
+
+                        <p class="font-avenir text-sm tracking-bit antialiased mb-8 font-thin">Av. de les Tres Creus, 2,
+                            46014
+                            València,
+                            Valencia, Spain</p>
+                        <p class="font-avenir text-sm tracking-bit antialiased mb-8 font-thin"><a
+                                href="mailto:example@btelabvalencia@gmail.com font-thin">btelabvalencia@gmail.com</a></p>
+                        <p class="font-avenir text-sm tracking-bit antialiased font-thin"><a href="tel:+963131893">963 131
+                                893</a></p>
+                    </div>
+
+                    <form>
+                        <div class="grid grid-cols-2 gap-4 md:pl-16 pt-24 md:pt-0">
+
+                            <div class="col-span-2 md:col-span-1">
+                                <label class="block">
+                                    <span class="font-avenir text-black antialiased font-thin">Name *</span>
+                                    <input type="text" placeholder="Enter your name" v-model="form.name" required class="mt-0 pb-0 font-avenir text-sm block
+                                               w-full
+                                                px-0.5
+                                                border-0 border-b-2 border-black
+                                                focus:ring-0 focus:border-black focus:border-2"></label>
+                            </div>
+                            <div class="col-span-2 md:col-span-1">
+                                <label class="block">
+                                    <span class="font-avenir text-black antialiased font-thin">Email *</span>
+                                    <input type="email" placeholder="Enter your email" v-model="form.email" required class="mt-0 pb-0 font-avenir text-sm block
+                                                w-full
+                                                px-0.5
+                                                border-0 border-b-2 border-black
+                                                focus:ring-0 focus:border-black focus:border-2" /></label>
+                            </div>
+                            <div class="col-span-2 md:col-span-1">
+                                <label class="block">
+                                    <span class="font-avenir text-black antialiased font-thin">Phone</span>
+                                    <input type="text" placeholder="Enter your phone" v-model="form.phone" class="mt-0 pb-0 font-avenir text-sm block
+                                                w-full
+                                                px-0.5
+                                                border-0 border-b-2 border-black
+                                                focus:ring-0 focus:border-black focus:border-2" /></label>
+                            </div>
+                            <div class="col-span-2 md:col-span-1">
+                                <label class="block">
+                                    <span class="font-avenir text-black antialiased font-thin">Address</span>
+                                    <input type="text" placeholder="Enter your address" v-model="form.address" class="mt-0 pb-0 block font-avenir text-sm
+                                                w-full
+                                                px-0.5
+                                                border-0 border-b-2 border-black
+                                                focus:ring-0 focus:border-black focus:border-2" /></label>
+                            </div>
+                            <div class="col-span-2">
+                                <label class="block">
+                                    <span class="font-avenir text-black antialiased font-thin">Subject *</span>
+                                    <input type="text" placeholder="Enter your subject" v-model="form.subject" required
+                                        class="mt-0 pb-0 block font-avenir text-sm
+                                                w-full
+                                                px-0.5
+                                                border-0 border-b-2 border-black
+                                                focus:ring-0 focus:border-black focus:border-2" /></label>
+                            </div>
+                            <div class="col-span-2">
+                                <label class="block">
+                                    <span class="font-avenir text-black antialiased font-thin">Message *</span>
+                                    <textarea name="message" cols="40" rows="3" v-model="form.message" class="mt-0 pb-0 block font-avenir text-sm
+                                                        w-full
+                                                        px-0.5
+                                                        border-0 border-b-2 border-black
+                                                        focus:ring-0 focus:border-black focus:border-2"></textarea></label>
+                            </div>
+                            <div class="col-span-2">
+                                <button type="submit"
+                                    class="w-full font-avenir text-sm bg-black text-white font-thin py-2 px-4 rounded mt-4">Submit</button>
+                            </div>
+
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-        <!-- <div class="w-full lg:w-1/2 p-4">
-        </div> -->
-       <!--  <div class="w-full lg:w-1/2 p-4">
-            <form>
-                <div>
-                    <label for="name">Name:</label>
-                    <input type="text" id="name" v-model="name" required />
-                </div>
-                <div>
-                    <label for="email">Email:</label>
-                    <input type="email" id="email" v-model="email" required />
-                </div>
-                <div>
-                    <label for="title">Title:</label>
-                    <input type="text" id="title" v-model="title" required/>
-                </div>
-                <div>
-                    <label for="text">Text:</label>
-                    <textarea id="text" v-model="text" required></textarea>
-                </div>
-                <button @click.prevent="submitForm">Submit</button>
-            </form>
-        </div> -->
     </div>
-
 </template>
 
 <script>
@@ -41,21 +98,25 @@ export default {
     name: "ContactForm",
     data() {
         return {
-            name: '',
-            email: '',
-            title: '',
-            text: ''
+            form: {
+                name: '',
+                email: '',
+                phone: '',
+                address: '',
+                subject: '',
+                message: ''
+            }
         };
     },
     methods: {
         submitForm() {
 
-            if (!this.name || !this.email || !this.title || !this.text) {
-                alert('Please fill all the fields');
+            if (!this.name || !this.email || !this.subject || !this.message) {
+                alert('Please fill all required fields');
                 return;
             }
-            
-            const mailto = `mailto:btelabvalencia@gmail.com?subject=${encodeURIComponent(this.title)}&body=From%3A%20${encodeURIComponent(this.name)}%20%28${encodeURIComponent(this.email)}%29%0D%0A%0D%0A${encodeURIComponent(this.text)}`;
+
+            const mailto = `mailto:btelabvalencia@gmail.com?subject=${encodeURIComponent(this.title)}&body=From%3A%20${encodeURIComponent(this.name)}%20%28${encodeURIComponent(this.email)}%29%0D%0A%0D%0A${encodeURIComponent(this.message)}`;
             window.location.href = mailto;
         }
     }
@@ -64,31 +125,31 @@ export default {
 
 <style>
 form {
-  display: flex;
-  flex-direction: column;
-  width: 500px;
-  margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    width: 500px;
+    margin: 0 auto;
 }
 
 label {
-  font-weight: bold;
+    font-weight: bold;
 }
 
 input,
 textarea {
-  padding: 10px;
-  margin-bottom: 20px;
-  font-size: 18px;
-  border-radius: 5px;
-  border: 1px solid gray;
+    padding: 10px;
+    margin-bottom: 20px;
+    font-size: 18px;
+    border-radius: 5px;
+    border: 1px solid gray;
 }
 
 button {
-  padding: 10px 20px;
-  background-color: blue;
-  color: white;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
+    padding: 10px 20px;
+    background-color: blue;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
 }
 </style>
